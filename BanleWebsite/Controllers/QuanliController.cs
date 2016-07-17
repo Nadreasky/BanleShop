8377251bdd23b838d63fd3de6b6f6a488345e283
treasuryonly <treasuryonly@gmail.com>
2016-07-17 16:54:20 +0700

    Hàm UpdateCategory
    mới làm display Category

--- BanleWebsite/Controllers/QuanliController.cs
+++ BanleWebsite/Controllers/QuanliController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BanleWebsite.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,9 +9,42 @@ namespace BanleWebsite.Controllers
 {
     public class QuanliController : Controller
     {
+        CategoryServices _categoryServices = new CategoryServices();
+
         // GET: Quanli
         public ActionResult Index()
         {
+            return View();
+        }
+
+        public ActionResult UpdateCategory()
+        {
+            List<Category> categoryList =_categoryServices.getAll();
+            ViewBag.categoryList = categoryList;
+
+            List<Category> categoryNoPreList = new List<Category>();
+            ViewBag.categoryNoPreList = categoryNoPreList;
+            foreach (var itemCategory in categoryList)
+            {
+                if (itemCategory.PreCateID == null)
+                {
+                    categoryNoPreList.Add(itemCategory);
+                }
+            }
+
+
+
+            //foreach (var itemCategoryNoPre in categoryNoPreList)
+            //{
+            //    foreach (var itemCategory in categoryList)
+            //    {
+            //        if (itemCategory.PreCateID == itemCategoryNoPre.ID) { }
+
+            //    }
+            //}
+
+            //Product p = null;
+            //ViewBag.pro1 = p;
             return View();
         }
     }
