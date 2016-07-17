7dd0ba6139fbf73e09c8a72ae8ab5c5dfb868f04
Nadreasky <nadreasky@gmail.com>
2016-07-17 11:44:45 +0700

    add quanli controller

---
+++ BanleWebsite/Controllers/QuanliController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BanleWebsite.Controllers
+{
+    public class QuanliController : Controller
+    {
+        // GET: Quanli
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
