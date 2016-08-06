f03916a7bfd19e3f54f42b92d2377f3fe33d7a62
Nadreasky <nadreasky@gmail.com>
2016-08-06 21:01:35 +0700

    update title page

--- BanleWebsite/Controllers/SanphamNoibatController.cs
+++ BanleWebsite/Controllers/SanphamNoibatController.cs
@@ -10,11 +10,20 @@ namespace BanleWebsite.Controllers
     public class SanphamNoibatController : Controller
     {
         ProductServices _productServices = new ProductServices();
+        ImageServices _imageServices = new ImageServices();
+        SizeProductDetailServices _sizeProductDetailServices = new SizeProductDetailServices();
+
         // GET: SanphamNoibat
         public ActionResult Index()
         {
             Product highlightProduct = _productServices.getHighlightProduct();
             ViewBag.highlightProduct = highlightProduct;
+
+            List<Image> colorImgProduct = _imageServices.getByProductId(highlightProduct.ID);
+            ViewBag.colorImgProduct = colorImgProduct;
+
+            List<SizeProductDetail> sizeProduct = _sizeProductDetailServices.getByProductId(highlightProduct.ID);
+            ViewBag.sizeProduct = sizeProduct;
             return View();
         }
     }
