0d267d58be5621160ec037ad4b25ebfadbc949dc
Nadreasky <nadreasky@gmail.com>
2016-08-10 15:07:47 +0700

    update mao

--- BanleWebsite/News.cs
+++ BanleWebsite/News.cs
@@ -23,22 +23,5 @@ namespace BanleWebsite
         public string MainImage { get; set; }
         public Nullable<int> Popular { get; set; }
         public Nullable<System.DateTime> CreateDate { get; set; }
-
-        public string GenerateSlug()
-        {
-            VietnameseSymbol vs = new VietnameseSymbol();
-            string phrase = string.Format("{0}-{1}", vs.ClearSymbol(Title), ID);
-            string str = RemoveAccent(phrase).ToLower();
-            str = Regex.Replace(str, @"[^a-z0-9\s-]", "");
-            str = Regex.Replace(str, @"\s+", " ").Trim();
-            str = str.Substring(0, str.Length <= 200 ? str.Length : 200).Trim();
-            str = Regex.Replace(str, @"\s", "-");
-            return str;
-        }
-        private string RemoveAccent(string text)
-        {
-            byte[] bytes = System.Text.Encoding.GetEncoding("Cyrillic").GetBytes(text);
-            return System.Text.Encoding.ASCII.GetString(bytes);
-        }
     }
 }
