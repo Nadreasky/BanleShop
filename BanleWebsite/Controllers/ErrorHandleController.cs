9ac104c08c012f0c11d6b7ee96f1e7365c3cf622
dacan2810 <star.02000@gmail.com>
2016-08-15 16:23:47 +0700

    Add error page
    Sua nut tiep tuc mua sam va add error page

---
+++ BanleWebsite/Controllers/ErrorHandleController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BanleWebsite.Controllers
+{
+    public class ErrorHandleController : Controller
+    {
+        // GET: ErrorHandle
+        public ActionResult Error()
+        {
+            return View();
+        }
+    }
+}
