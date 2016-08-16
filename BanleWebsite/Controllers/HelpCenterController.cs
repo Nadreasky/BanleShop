27991638b6ac5bc2958f277172c472e670a43d2d
Nadreasky <nadreasky@gmail.com>
2016-08-16 22:09:42 +0700

    update tab for quytac

---
+++ BanleWebsite/Controllers/HelpCenterController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BanleWebsite.Controllers
+{
+    public class HelpCenterController : Controller
+    {
+        // GET: HelpCenter
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Quytactrahang()
+        {
+            return View();
+        }
+    }
+}
